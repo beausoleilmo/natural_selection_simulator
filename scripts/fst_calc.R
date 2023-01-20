244e9e21b89fb4594236d7b5ad22b5a61b7de781
beausoleilmo <beausoleilmo@gmail.com>
2023-01-20 15:58:49 -0500

    added data from hartl

--- scripts/fst_calc.R
+++ scripts/fst_calc.R
@@ -3,7 +3,7 @@ n.sub = 4
 n = 400 * n.sub
 # set.seed(12)
 pop.geno = data.frame(geno = rbinom(n = n,size = 2, prob = .1), 
-           sub.pop = factor(rep(x = LETTERS[1:n.sub], each = n/n.sub)))
+                      sub.pop = factor(rep(x = LETTERS[1:n.sub], each = n/n.sub)))
 probs = c(.1,.2,.8,.75)
 # pop.geno$geno = rep(x = c(1,2,0,1), each = n/n.sub)
 pop.geno$geno = ifelse(pop.geno$sub.pop == "A",rbinom(n = n,size = 2, prob = probs[1]),pop.geno$geno)
@@ -37,32 +37,44 @@ fst.cal <- function(hs, ht) {
   fst = (ht-(hs))/ht
   return(fst)
 }
+fst.cal <- function(h1, h2) {
+  fst = (h2-(h1))/h2
+  return(fst)
+}
+
 # fst.cal(hs = 0.1424, ht = 0.2371)
 
 fst.cal(hs = mean(geno.table$hs), ht = ht)
 
 ###
-hert= c(0.4893,
-        0.4058,
-        0.5000,
-        0.4507,
-        0.4216,
-        0.4482,
-        0.0000,
-        0.0620,
-        0.0139,
-        0.0159,
-        0.0100,
-        0.0178,
-        0.0100,
-        0.0198,
-        0.1268,
-        0.0040,
-        0.0080,
-        0.2202,
-        0.1895,
-        0.3476,
-        0.4842,
-        0.0276)
 
-sum(hert)/30
+all.fq = c(0.573, 0.717, 0.504, 0.657, 0.302, 0.339, 
+           0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000,
+           0.032, 0.007, 0.008, 0.005, 0.009, 0.005, 0.010, 0.068, 0.002, 0.004, 0.126, 
+           0.106, 0.224, 0.411, 0.014)
+hert = c(0.4893, 0.4058, 0.5000, 0.4507, 0.4216, 0.4482, 0.0000,
+         0.0000, 0.0000, 0.0000, 0.0000, 0.0000,
+         0.0000, 0.0000, 0.0000, 0.0620, 0.0139, 0.0159, 0.0100, 
+         0.0178, 0.0100, 0.0198, 0.1268, 0.0040, 0.0080, 0.2202,
+         0.1895, 0.3476, 0.4842, 0.0276)
+region = c(rep("W", each = 6),
+           rep("C", each = 20),
+           rep("E", each = 4))
+table.4.1 = data.frame(region, all.fq, hert)
+
+
+region.sub = table.4.1 %>% 
+  mutate(my.het = 2*all.fq*(1-all.fq)) %>% 
+  group_by(region) %>% 
+  summarise(mean.all.fq = mean(all.fq),
+            mean.het = 2*mean.all.fq*(1-mean.all.fq),
+            nb = n())
+
+hs = sum(table.4.1$hert)/nrow(table.4.1)
+hr = sum(region.sub$nb*region.sub$mean.het)/sum(region.sub$nb)
+# Heterozygosity FROM the allele FQ (expect) considering the TOTAL population (without subdivision)
+ht = 2*mean(table.4.1$all.fq)*(1-mean(table.4.1$all.fq))
+
+fst.cal(hr, ht)
+fst.cal(hs, ht)
+fst.cal(hs, hr)
